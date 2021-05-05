3172116fac199322ad735e38dd190696836b0e0e
elpatronaco <pau.colome@between.tech>
2021-05-05 17:33:53 +0200

    Added controls to selectionplanet form and mapped planet info to controls

--- PersonalControls/SelectionPlanet.Designer.cs
+++ PersonalControls/SelectionPlanet.Designer.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Windows.Forms;
 
 namespace PersonalControls
 {
@@ -33,13 +34,28 @@ namespace PersonalControls
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
-            this.pb_planet = new System.Windows.Forms.PictureBox();
+            this.lb_routes = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbl_natives = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_filiation = new System.Windows.Forms.Label();
+            this.lbl_situation = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_sector = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pb_planet_image = new System.Windows.Forms.PictureBox();
+            this.cbx_destination_planet = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.cbx_planets = new System.Windows.Forms.ComboBox();
+            this.cbx_origin_planet = new System.Windows.Forms.ComboBox();
             this.label10 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel7.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.pb_planet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pb_planet_image)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,43 +73,212 @@ namespace PersonalControls
             // 
             this.panel7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel7.Controls.Add(this.pb_planet);
+            this.panel7.Controls.Add(this.lb_routes);
+            this.panel7.Controls.Add(this.label9);
+            this.panel7.Controls.Add(this.label11);
+            this.panel7.Controls.Add(this.lbl_natives);
+            this.panel7.Controls.Add(this.label6);
+            this.panel7.Controls.Add(this.lbl_filiation);
+            this.panel7.Controls.Add(this.lbl_situation);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Controls.Add(this.label8);
+            this.panel7.Controls.Add(this.lbl_sector);
+            this.panel7.Controls.Add(this.label5);
+            this.panel7.Controls.Add(this.lbl_name);
+            this.panel7.Controls.Add(this.label3);
+            this.panel7.Controls.Add(this.pb_planet_image);
+            this.panel7.Controls.Add(this.cbx_destination_planet);
+            this.panel7.Controls.Add(this.label2);
             this.panel7.Controls.Add(this.label1);
-            this.panel7.Controls.Add(this.cbx_planets);
+            this.panel7.Controls.Add(this.cbx_origin_planet);
             this.panel7.Controls.Add(this.label10);
             this.panel7.Location = new System.Drawing.Point(40, 30);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(1057, 995);
             this.panel7.TabIndex = 6;
             // 
-            // pb_planet
+            // lb_routes
             // 
-            this.pb_planet.Location = new System.Drawing.Point(38, 202);
-            this.pb_planet.Name = "pb_planet";
-            this.pb_planet.Size = new System.Drawing.Size(441, 306);
-            this.pb_planet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pb_planet.TabIndex = 14;
-            this.pb_planet.TabStop = false;
+            this.lb_routes.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb_routes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lb_routes.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lb_routes.ForeColor = System.Drawing.Color.White;
+            this.lb_routes.FormattingEnabled = true;
+            this.lb_routes.ItemHeight = 25;
+            this.lb_routes.Location = new System.Drawing.Point(21, 669);
+            this.lb_routes.Name = "lb_routes";
+            this.lb_routes.Size = new System.Drawing.Size(373, 75);
+            this.lb_routes.TabIndex = 30;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(18, 666);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(373, 42);
+            this.label9.TabIndex = 29;
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(19, 643);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(375, 23);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Routes";
+            this.label11.Visible = false;
+            // 
+            // lbl_natives
+            // 
+            this.lbl_natives.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_natives.ForeColor = System.Drawing.Color.White;
+            this.lbl_natives.Location = new System.Drawing.Point(18, 601);
+            this.lbl_natives.Name = "lbl_natives";
+            this.lbl_natives.Size = new System.Drawing.Size(373, 42);
+            this.lbl_natives.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(19, 578);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(375, 23);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Natives";
+            this.label6.Visible = false;
+            // 
+            // lbl_filiation
+            // 
+            this.lbl_filiation.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_filiation.ForeColor = System.Drawing.Color.White;
+            this.lbl_filiation.Location = new System.Drawing.Point(19, 471);
+            this.lbl_filiation.Name = "lbl_filiation";
+            this.lbl_filiation.Size = new System.Drawing.Size(373, 42);
+            this.lbl_filiation.TabIndex = 25;
+            // 
+            // lbl_situation
+            // 
+            this.lbl_situation.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_situation.ForeColor = System.Drawing.Color.White;
+            this.lbl_situation.Location = new System.Drawing.Point(19, 536);
+            this.lbl_situation.Name = "lbl_situation";
+            this.lbl_situation.Size = new System.Drawing.Size(373, 42);
+            this.lbl_situation.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(18, 513);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(375, 23);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Situation";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(18, 448);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(375, 23);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Filiation";
+            this.label8.Visible = false;
+            // 
+            // lbl_sector
+            // 
+            this.lbl_sector.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_sector.ForeColor = System.Drawing.Color.White;
+            this.lbl_sector.Location = new System.Drawing.Point(18, 406);
+            this.lbl_sector.Name = "lbl_sector";
+            this.lbl_sector.Size = new System.Drawing.Size(373, 42);
+            this.lbl_sector.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(19, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(375, 23);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Sector";
+            this.label5.Visible = false;
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lbl_name.ForeColor = System.Drawing.Color.White;
+            this.lbl_name.Location = new System.Drawing.Point(18, 341);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(373, 42);
+            this.lbl_name.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(19, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(375, 23);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Name";
+            this.label3.Visible = false;
+            // 
+            // pb_planet_image
+            // 
+            this.pb_planet_image.Location = new System.Drawing.Point(413, 126);
+            this.pb_planet_image.Name = "pb_planet_image";
+            this.pb_planet_image.Size = new System.Drawing.Size(615, 837);
+            this.pb_planet_image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pb_planet_image.TabIndex = 16;
+            this.pb_planet_image.TabStop = false;
+            // 
+            // cbx_destination_planet
+            // 
+            this.cbx_destination_planet.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.cbx_destination_planet.FormattingEnabled = true;
+            this.cbx_destination_planet.Location = new System.Drawing.Point(18, 255);
+            this.cbx_destination_planet.Name = "cbx_destination_planet";
+            this.cbx_destination_planet.Size = new System.Drawing.Size(375, 40);
+            this.cbx_destination_planet.TabIndex = 15;
+            this.cbx_destination_planet.SelectionChangeCommitted += new System.EventHandler(this.cbx_planets_SelectionChangeCommitted);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(18, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(374, 36);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Destination";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(54, 144);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(19, 126);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 29);
+            this.label1.Size = new System.Drawing.Size(374, 35);
             this.label1.TabIndex = 13;
-            this.label1.Text = "Origin:";
+            this.label1.Text = "Origin";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // cbx_planets
+            // cbx_origin_planet
             // 
-            this.cbx_planets.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.cbx_planets.FormattingEnabled = true;
-            this.cbx_planets.Location = new System.Drawing.Point(169, 142);
-            this.cbx_planets.Name = "cbx_planets";
-            this.cbx_planets.Size = new System.Drawing.Size(286, 37);
-            this.cbx_planets.TabIndex = 12;
-            this.cbx_planets.SelectionChangeCommitted += new System.EventHandler(this.cbx_planets_SelectionChangeCommitted);
+            this.cbx_origin_planet.Font = new System.Drawing.Font("Microsoft JhengHei UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.cbx_origin_planet.FormattingEnabled = true;
+            this.cbx_origin_planet.Location = new System.Drawing.Point(19, 164);
+            this.cbx_origin_planet.Name = "cbx_origin_planet";
+            this.cbx_origin_planet.Size = new System.Drawing.Size(374, 40);
+            this.cbx_origin_planet.TabIndex = 12;
             // 
             // label10
             // 
@@ -118,18 +303,40 @@ namespace PersonalControls
             this.Load += new System.EventHandler(this.OnFormLoad);
             this.panel1.ResumeLayout(false);
             this.panel7.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize) (this.pb_planet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pb_planet_image)).EndInit();
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.PictureBox pb_planet;
+        private System.Windows.Forms.ListBox lb_routes;
 
-        private System.Windows.Forms.ComboBox cbx_planets;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label8;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label lbl_name;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.PictureBox pb_planet_image;
+
+        private System.Windows.Forms.ComboBox cbx_destination_planet;
+        private System.Windows.Forms.ComboBox cbx_origin_planet;
+
+        private System.Windows.Forms.Label label2;
 
         private System.Windows.Forms.Label label1;
 
-        private System.Windows.Forms.ComboBox comboBox1;
-
+        private System.Windows.Forms.Label lbl_sector;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_filiation;
+        private System.Windows.Forms.Label lbl_situation;
+        private System.Windows.Forms.Label lbl_natives;
+        
         #endregion
 
         private System.Windows.Forms.Panel panel1;
--- PersonalControls/SelectionPlanet.cs
+++ PersonalControls/SelectionPlanet.cs
@@ -30,28 +30,66 @@ namespace PersonalControls
 
             if (!(_planets.Count > 0)) return;
 
-            cbx_planets.Items.Clear();
+            cbx_origin_planet.Items.Clear();
+            cbx_destination_planet.Items.Clear();
+
             foreach (var planet in _planets)
-                cbx_planets.Items.Add(planet.Name);
+            {
+                cbx_origin_planet.Items.Add(planet.Name);
+                cbx_destination_planet.Items.Add(planet.Name);
+            }
         }
 
         private void cbx_planets_SelectionChangeCommitted(object sender, EventArgs e)
         {
-            var selectedPlanet = _planets[cbx_planets.SelectedIndex];
+            var selectedPlanet = _planets[cbx_destination_planet.SelectedIndex];
 
-            var imagePath = selectedPlanet.ImageName != null ? Path.Combine(Application.StartupPath, 
-                "assets", 
-                "planetes",
-                selectedPlanet.ImageName) : null;
-            
-            pb_planet.Image = Image.FromFile(imagePath ?? Path.Combine
+            MapPlanetToControls(selectedPlanet);
+        }
+
+        private void MapPlanetToControls(Planet planet)
+        {
+            if (!label3.Visible)
+                // var labels = new[] {label3, label5, label6, label7, label8, label11};
+                // foreach (var label in labels)
+                //     label.Visible = true;
+                foreach (var label in panel7.Controls.OfType<Label>())
+                    if (!label.Visible)
+                        label.Visible = true;
+
+            lbl_name.Text = planet.Name;
+            lbl_sector.Text = planet.Sector;
+            lbl_filiation.Text = planet.Filiation;
+            lbl_situation.Text = $@"{planet.Situation.Latitude}, {planet.Situation.Longitude}";
+            lbl_natives.Text = planet.Natives;
+
+            lb_routes.Items.Clear();
+            foreach (var route in planet.HyperspaceRoutes)
+                lb_routes.Items.Add(route);
+
+            var imagePath = planet.ImageName != null
+                ? Path.Combine(Application.StartupPath,
+                    "assets",
+                    "planetes",
+                    planet.ImageName)
+                : null;
+
+            pb_planet_image.Image = Image.FromFile(imagePath ?? Path.Combine
                 ("assets", "placeholder.png"));
+
+            // fit image in picturebox
+            var imageSize = pb_planet_image.Image.Size;
+            var fitSize = pb_planet_image.ClientSize;
+            pb_planet_image.SizeMode =
+                imageSize.Width > fitSize.Width || imageSize.Height > fitSize.Height
+                    ? PictureBoxSizeMode.Zoom
+                    : PictureBoxSizeMode.CenterImage;
         }
 
         private static List<Planet> ReadPlanets(string dbName)
         {
             var dbPath = Path.Combine("assets", dbName);
-            
+
             var doc = new XmlDocument();
             doc.Load(dbPath);
 
@@ -71,7 +109,7 @@ namespace PersonalControls
                 let routes = planet.SelectNodes($"//planet[name=\"{name}\"]/hyperspaceRoute/route")
                 let hyperSpaceRoutes =
                     (from XmlNode route in routes select route.InnerText).ToList()
-                let imageName = FindFileByName(Path.Combine(Application.StartupPath, 
+                let imageName = FindFileByName(Path.Combine(Application.StartupPath,
                     "assets", "planetes"), name)
                 select new Planet
                 {
@@ -89,7 +127,7 @@ namespace PersonalControls
         {
             var dir = new DirectoryInfo(path);
             var files = dir.GetFiles($@"{name}*", SearchOption.TopDirectoryOnly);
-            return files != null && files.Length > 0 ? files[0].Name : null;
+            return files is {Length: > 0} ? files[0].Name : null;
         }
     }
 }
